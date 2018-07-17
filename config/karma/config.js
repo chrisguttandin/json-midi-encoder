f01a903594b2aeaaca80d4cf526ff998c71049e3
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-07-17 19:29:52 +0200

    style: apply linting rules

--- config/karma/config.js
+++ config/karma/config.js
@@ -1,3 +1,5 @@
+const { env } = require('process');
+
 module.exports = (config) => {
 
     config.set({
@@ -45,7 +47,7 @@ module.exports = (config) => {
 
     });
 
-    if (process.env.TRAVIS) {
+    if (env.TRAVIS) {
 
         config.set({
 
@@ -69,7 +71,7 @@ module.exports = (config) => {
                 }
             },
 
-            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+            tunnelIdentifier: env.TRAVIS_JOB_NUMBER
 
         });
 
