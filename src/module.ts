c6a07c1620e452e45101113a19ee3a570be52037
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-08-28 01:47:09 +0200

    fix: remove inferable types

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { IMidiFile, load } from 'json-midi-encoder-broker';
+import { load } from 'json-midi-encoder-broker';
 import { worker } from './worker/worker';
 
 const blob: Blob = new Blob([ worker ], { type: 'application/javascript; charset=utf-8' });
@@ -7,7 +7,6 @@ const url: string = URL.createObjectURL(blob);
 
 const jsonMidiEncoder = load(url);
 
-// @todo Out of some reason the encode() function has to be typed again.
-export const encode: (midiFile: IMidiFile) => Promise<ArrayBuffer> = jsonMidiEncoder.encode;
+export const encode = jsonMidiEncoder.encode;
 
 URL.revokeObjectURL(url);
