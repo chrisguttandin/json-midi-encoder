48a69ad51de8bd1e2fab49f803c7a0ad00eb110f
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-02-16 21:28:53 +0100

    test: remove text file fix

--- test/helper/load-fixture.js
+++ test/helper/load-fixture.js
@@ -1,14 +1,3 @@
-function base64ToArrayBuffer (encodedData) {
-    const decodedData = atob(encodedData.replace(/\s/g, ''));
-    const uint8Array = new Uint8Array(decodedData.length);
-
-    Array.prototype.forEach.call(uint8Array, function (value, index) {
-        uint8Array[index] = decodedData.charCodeAt(index);
-    });
-
-    return uint8Array.buffer;
-}
-
 export const loadFixtureAsArrayBuffer = (fixture, callback) => {
     const request = new XMLHttpRequest();
 
@@ -16,18 +5,10 @@ export const loadFixtureAsArrayBuffer = (fixture, callback) => {
         callback('request-failed');
     };
     request.onload = function (event) {
-        if (fixture.slice(-4) === '.txt') {
-            callback(null, base64ToArrayBuffer(event.target.response));
-        } else {
-            callback(null, event.target.response);
-        }
+        callback(null, event.target.response);
     };
-    request.open('GET', 'base/test/fixtures/' + fixture);
-
-    if (fixture.slice(-4) !== '.txt') {
-        request.responseType = 'arraybuffer';
-    }
-
+    request.open('GET', '/base/test/fixtures/' + fixture);
+    request.responseType = 'arraybuffer';
     request.send();
 };
 
@@ -44,6 +25,6 @@ export const loadFixtureAsJson = (fixture, callback) => {
             callback('request-failed');
         }
     };
-    request.open('GET', 'base/test/fixtures/' + fixture);
+    request.open('GET', '/base/test/fixtures/' + fixture);
     request.send();
 };
--- test/unit/module.js
+++ test/unit/module.js
@@ -5,22 +5,22 @@ describe('module', () => {
 
     describe('encode()', () => {
 
-        leche.withData({
-            'MIDIOkFormat1-lyrics': [ 'MIDIOkFormat1-lyrics.mid', 'MIDIOkFormat1-lyrics.json' ],
-            'MIDIOkFormat2': [ 'MIDIOkFormat2.mid', 'MIDIOkFormat2.json' ],
-            'SubTractor 1': [ 'SubTractor 1.mid.txt', 'SubTractor 1.json' ],
-            'SubTractor 2': [ 'SubTractor 2.mid', 'SubTractor 2.json' ],
-            'because': [ 'because.mid', 'because.json' ],
-            'scale': [ 'scale.mid', 'scale.json' ]
-        }, (midiFilename, jsonFilename) => {
+        leche.withData([
+            [ 'because' ],
+            [ 'MIDIOkFormat1-lyrics' ],
+            [ 'MIDIOkFormat2' ],
+            [ 'scale' ],
+            [ 'SubTractor 1' ],
+            [ 'SubTractor 2' ]
+        ], (filename) => {
 
             it('should encode the json object', function (done) {
                 this.timeout(4000);
 
-                loadFixtureAsArrayBuffer(midiFilename, (err, arrayBuffer) => {
+                loadFixtureAsArrayBuffer(filename + '.mid', (err, arrayBuffer) => {
                     expect(err).to.be.null;
 
-                    loadFixtureAsJson(jsonFilename, (rr, json) => {
+                    loadFixtureAsJson(filename + '.json', (rr, json) => {
                         expect(rr).to.be.null;
 
                         jsonMidiEncoder
@@ -37,7 +37,7 @@ describe('module', () => {
             it('should refuse to encode a none json object', function (done) {
                 this.timeout(4000);
 
-                loadFixtureAsArrayBuffer(midiFilename, (err, arrayBuffer) => {
+                loadFixtureAsArrayBuffer(filename + '.mid', (err, arrayBuffer) => {
                     expect(err).to.be.null;
 
                     jsonMidiEncoder
