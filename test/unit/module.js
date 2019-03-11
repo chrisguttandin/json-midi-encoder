3e4de27aa5542c6ebe7c4a7fc9ddcb3ecf6e25b1
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-03-11 18:16:13 +0100

    test: add additional fixture

--- test/unit/module.js
+++ test/unit/module.js
@@ -15,6 +15,7 @@ describe('module', () => {
             [ 'scale' ],
             [ 'SubTractor 1' ],
             [ 'SubTractor 2' ],
+            [ 'test' ],
             [ 'test8bars' ]
         ], (filename) => {
 
